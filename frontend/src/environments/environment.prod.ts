e658e132b14f773f13205b878612a7ff39d0c7af
Harsimran0692 <harsimran061992@gmail.com>
2026-05-15 06:17:35 -0400

    changing backend production url in frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-api-domain.com/api',
+  apiUrl: 'https://shophub-nfyt.onrender.com/api/',
   googleClientId:
     '134919760637-7sa4p3otrnfr5hj685ru3qd39ue1f71i.apps.googleusercontent.com',
 };
