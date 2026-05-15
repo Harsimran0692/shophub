305ab03ce846847cc1432c48a875c3e495ac6114
Harsimran0692 <harsimran061992@gmail.com>
2026-05-15 06:13:31 -0400

    Redis host name change

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,7 +1,7 @@
 import Redis from "ioredis";
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || "localhost",
+  host: process.env.REDIS_URL || "localhost",
   port: process.env.REDIS_PORT || 6379,
   retryStrategy: (times) => Math.min(times * 50, 2000),
   maxRetriesPerRequest: 3,
